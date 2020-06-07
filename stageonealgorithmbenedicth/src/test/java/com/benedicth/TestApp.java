1d20b0dd5afb243c38e079ec5e3a505637a0fe16
hbnettech <ben360degree@gmail.com>
2020-06-07 14:31:31 +0100

    added more unit testing

--- stageonealgorithmbenedicth/src/test/java/com/benedicth/TestApp.java
+++ stageonealgorithmbenedicth/src/test/java/com/benedicth/TestApp.java
@@ -10,6 +10,8 @@ public class TestApp {
 
     App app = null;
     Integer noOfWashes = 0;
+    Integer[] cleanPile = {1, 3, 2, 4, 3, 1, 2, 1, 1};
+    Integer[] dirtyPile = {1, 4, 3, 1, 5, 9, 10, 9, 3, 2, 4};
 
     @Before
     public void setUp(){
@@ -27,11 +29,41 @@ public class TestApp {
         assertTrue( (noOfWashes >= 0) && (noOfWashes <= 50));
     }
 
+    @Test
+    public void shouldAnswerTrueIfCleanPileWithinRangeOneAndFifty()
+    {
+        boolean found = true;
+        for (int i = 0; i < cleanPile.length; i++)
+        {
+            if (cleanPile[ i ]  <= 0 || cleanPile[ i ]  > 50 )
+            {
+                found = false;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
+    @Test
+    public void shouldAnswerTrueIfDirtyPileWithinRangeOneAndFifty()
+    {
+        boolean found = true;
+        for (int i = 0; i < dirtyPile.length; i++)
+        {
+            if (dirtyPile[ i ]  <= 0 || dirtyPile[ i ]  > 50 )
+            {
+                found = false;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
 
     @Test
     public  void testMaxNumberPairsShouldReturnMaxPairs(){
         Integer expect = 5;
-        assertEquals(expect,app.maxNumberPairs(noOfWashes,new Integer[]{1, 3, 2, 4, 3, 1, 2, 1, 1},new Integer[]{1, 4, 3, 1, 5, 9, 10, 9, 3, 2, 4}));
+        assertEquals(expect,app.maxNumberPairs(noOfWashes,cleanPile,dirtyPile));
     }
 
 }
